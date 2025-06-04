4c16c33bfeaa2e6253022d2731d59ab5ffc1bd47
nguyen.thaotra <nguyen.trathao@asnet.com.vn>
2025-06-04 10:17:55 +0700

    feat: enhance dashboard page with revenue and invoice components

--- nextjs-dashboard/app/page.tsx
+++ nextjs-dashboard/app/page.tsx
@@ -23,8 +23,9 @@ export default function Page() {
             </a>
             , brought to you by Vercel.
           </p>
+          {/* TODO: Navigate to the login page */}
           <Link
-            href="/login"
+            href="/dashboard"
             className="flex items-center gap-5 self-start rounded-lg bg-blue-500 px-6 py-3 text-sm font-medium text-white transition-colors hover:bg-blue-400 md:text-base"
           >
             <span>Log in</span> <ArrowRightIcon className="w-5 md:w-6" />
